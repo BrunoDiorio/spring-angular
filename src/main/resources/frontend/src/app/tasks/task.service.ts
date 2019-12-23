9dc98ce8b81bf33b800ec50242b8a16936127d1e
Bruno Diorio <bruno.diorio@live.com>
2019-12-22 22:03:35 -0300

    Consumindo spring

---
+++ src/main/resources/frontend/src/app/tasks/task.service.ts
@@ -0,0 +1,15 @@
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {map} from "rxjs/operators";
+
+@Injectable()
+export class TaskService{
+
+    constructor(
+        private http: HttpClient) {
+    }
+
+    getTasks(){
+        return this.http.get('api/tasks');
+    }
+}
--- src/main/resources/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/resources/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Task} from "../task.model";
+import {TaskService} from "../task.service";
 
 @Component({
-  selector: 'app-tasks-list',
-  templateUrl: './tasks-list.component.html',
-  styleUrls: ['./tasks-list.component.css']
+    selector: 'app-tasks-list',
+    templateUrl: './tasks-list.component.html',
+    styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
 
-  constructor() { }
+    tasks: Task[] = [];
 
-  ngOnInit() {
-  }
+    constructor(
+        private taskService: TaskService) {
+    }
+
+    ngOnInit() {
+        // this.tasks.push(new Task(1,"Task1", true, "01/12/2019"));
+        return this.taskService.getTasks()
+            .subscribe(
+                (tasks: any[]) =>{
+                    this.tasks = tasks;
+                },
+                (error) => console.log(error));
+    }
+
+    getDueDateLabel(task: Task){
+        return task.completed ? 'badge-success' : 'badge-primary';
+    }
+
+    onTaskChange(event, task: Task){
+        task.completed = !task.completed;
+        console.log(task);
+    }
 
 }
